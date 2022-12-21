8dcd1b16e9807f66cc2919a03926f6504a17cfb6
Michael Murithi Kariuki <michael.kariuki@siemens-healthineers.com>
2022-12-21 22:18:35 +0100

    learn(apii): Learnt how to add middleware

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,14 @@
+import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 import router from './router';
 
 const app = express();
 
+app.use(cors());
 app.use(morgan(('dev')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.status(200);
