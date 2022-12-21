13666e1c0da3aade8d430e5732249f26a0e02132
Michael Murithi Kariuki <michael.kariuki@siemens-healthineers.com>
2022-12-21 21:55:42 +0100

    learn(api): Added express routes

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import router from './router';
 
 const app = express();
 
 app.get('/', (req, res) => {
     console.log('Executing root endpoint');
     res.status(200);
-    res.json({ message: 'Hello from express API' });
+    res.json({ message: 'Hello express product logs API is working' });
 });
+
+app.use('/api', router);
 
 export default app;
