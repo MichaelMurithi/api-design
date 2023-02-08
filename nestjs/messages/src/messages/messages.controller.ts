92653dff29f29ecd0306f068914c7ea45d7e9696
Michael Murithi Kariuki <michael.kariuki@siemens-healthineers.com>
2023-02-08 23:14:09 +0100

    learn(nestjs/Messages): learnt dependency injection

--- nestjs/messages/src/messages/messages.controller.ts
+++ nestjs/messages/src/messages/messages.controller.ts
@@ -1,22 +1,27 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, NotFoundException, Param, Post } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+    constructor(private messagesService: MessagesService) { }
+
     @Get()
     listMessages() {
-        return 'Message';
+        return this.messagesService.findAll();
     }
 
     @Post()
     createMessage(@Body() body: CreateMessageDto) {
-        return { body }
+        return this.messagesService.create(body.content);
     }
 
     @Get('/:id')
-    getMessage(@Param('id') id: string) {
-        console.log('The id is', id);
+    async getMessage(@Param('id') id: string) {
+        const message = await this.messagesService.findOne(id);
 
-        return 'Getting message...'
+        if (!message) {
+            throw new NotFoundException('Message not found');
+        }
     }
 }
