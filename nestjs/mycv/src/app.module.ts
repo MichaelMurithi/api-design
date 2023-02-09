9cc42aa5bcbf37c07b8e6b514e81a7e8492f6058
Michael Murithi Kariuki <michael.kariuki@siemens-healthineers.com>
2023-02-09 19:20:18 +0100

    practice(nestjs/mycv): setup modules and services for mycv app

--- nestjs/mycv/src/app.module.ts
+++ nestjs/mycv/src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -11,6 +13,8 @@ import { AppService } from './app.service';
       entities: [],
       synchronize: true,
     }),
+    UsersModule,
+    ReportsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
