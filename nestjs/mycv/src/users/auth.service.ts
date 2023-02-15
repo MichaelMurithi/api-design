b3497e3e19564587313072ebb4474e5ff98577c8
Michael Murithi Kariuki <michael.kariuki@siemens-healthineers.com>
2023-02-15 21:22:12 +0100

    learn(nestjs): implemented sign up and password encryption

--- nestjs/mycv/src/users/auth.service.ts
+++ nestjs/mycv/src/users/auth.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { BadRequestException } from '@nestjs/common/exceptions';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
+import { promisify } from 'util';
+import { UsersService } from './users.service';
+
+const scrypt = promisify(_scrypt);
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private usersService: UsersService) { }
+
+    async signUp(email: string, password: string) {
+        // See if email is in use
+        const users = await this.usersService.find(email);
+
+        if (users.length) {
+            throw new BadRequestException('Email in use');
+        }
+
+        //Hash the users password
+        const salt = randomBytes(8).toString('hex');
+        const hash = (await scrypt(password, salt, 32)) as Buffer;
+        const result = salt + '.' + hash.toString('hex');
+
+        //Create a new user and save it
+        return await this.usersService.create(email, result);
+    }
+
+    signin() {
+
+    }
+}
